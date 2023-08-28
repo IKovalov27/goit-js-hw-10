12d5fabbd4e2279ac67dec3c6bd98a32add29cff
Illia Kovalov <kovalev.illia2009@gmail.com>
2023-08-28 21:40:39 +0200

    27

---
+++ src/js/03-promises.js
@@ -0,0 +1,45 @@
+import Notiflix from 'notiflix';
+
+
+const formEl = document.querySelector('.form');
+formEl.addEventListener('submit', onFormSubmit);
+
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  let delay = Number(event.target.elements.delay.value);
+  const step = Number(event.target.elements.step.value);
+  const amount = Number(event.target.elements.amount.value);
+
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
+  }
+}
